35d40d455af5792f8ce167f6bea4fccf3cdefa07
tspurway <tspurway@gmail.com>
2015-05-06 16:50:23 -0400

    adding an IP click counter Inferno rule to catch fraudulent (ie. repetitive) clicking above threshold.  The job runs daily and reports Events through datadog.statsd

--- config_infernyx/__init__.py
+++ config_infernyx/__init__.py
@@ -1,10 +1,20 @@
 
 # hint: there is a geoip database in infernyx/etc (https://github.com/tspurway/infernyx/tree/master/etc)
-GEOIP = "/<path>/<to>/GeoLite2-Country.mmdb"
+GEOIP = "/Users/tspurway/dev/infernyx/etc/GeoLite2-Country.mmdb"
 PG_PASSWORD = ''
 RS_BUCKET = ''
-RS_DB = ''
-RS_HOST = ''
+RS_DB = 'tiles'
+RS_HOST = 'localhost'
+RS_PORT = 5454
 RS_PASSWORD = ''
 RS_USER = ''
 STATSD = {'host': 'localhost', 'port': 8125}
+DASH_KEY_ID = 'AKIAJQER5UY7HK5YDZ5A'
+DASH_ACCESS_KEY = 'cMPOn0BuwPxIPAx+71xUf5xASdlIikzgqTQo79x/'
+DASH_BUCKET = 'net-mozaws-prod-metrics-data'
+DASH_USER = 'read_only'
+DASH_PASSWORD = 'gohd0oon7iX6ooSi7gai'
+APP_MAX_BLOBS = 100000
+APP_MIN_BLOBS = 1
+IMPRESSION_MAX_BLOBS = 100000
+IMPRESSION_MIN_BLOBS = 1
